59b12aecbe26e62d60aa79d75bd9f4f0143723f5
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2023-07-05 09:37:12 -0600

    endpoint
    endpoint Prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-
+app.post('/ultramsgwebhook', (req, res) => {
+    console.log(req.body) // print all response
+    //messageFrom=req.body['data']['from'] // sender number
+    //messageMsg=req.body['data']['body'] // Message text
+    res.status(200).end()
+  })
+  
 
 
 export default app;
